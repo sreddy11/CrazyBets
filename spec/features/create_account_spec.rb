54dac7ab07016a5fe439a8bd19e237764bb6eca9
sreddy11 <sreddy1.sr@gmail.com>
2013-07-23 16:46:35 -0400

    Fixed Spec Tests

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -11,39 +11,39 @@ describe "Creating an account" do
       fill_in "First Name", :with => 'sid'
       fill_in "Last Name", :with => 'reddy'
       fill_in "User Name", :with => 'sreddy1'
-      fill_in "Password", :with => "password"
+      fill_in "user[password]", :with => "password"
       fill_in "Confirm Password", :with => "password"
       fill_in "E-mail", :with => "email@yahoo.com"
       click_button "Sign Up"
     end
 
     it "creates the account" do
-      current_path.should == '/users/sid'
+      current_path.should == '/users/sreddy1'
       page.should have_content("Your Account (sreddy1) has been successfully created")
     end
+  end
   
-    context "invalid form" do
+  context "invalid form" do
 
-      before do 
-        visit '/users/new'
-        fill_in "First Name", :with => ''
-        fill_in "Last Name", :with => ''
-        fill_in "User Name", :with => 'sreddy1'
-        fill_in "Password", :with => "pass"
-        fill_in "Confirm Password", :with => "pass2"
-        fill_in "E-mail", :with => "email"
-        click_button "Create User"
-      end
+    before do 
+      visit '/users/new'
+      fill_in "First Name", :with => ''
+      fill_in "Last Name", :with => ''
+      fill_in "User Name", :with => ''
+      fill_in "user[password]", :with => "pass"
+      fill_in "Confirm Password", :with => "pass2"
+      fill_in "E-mail", :with => "email"
+      click_button "Sign Up"
+    end
 
-      it "gives new errors" do
-        current_path.should == '/users'
-        page.should have_content("First Name can't be blank")
-        page.should have_content("Last Name can't be blank")
-        page.should have_content("User name has already been taken")
-        page.should have_content("Password is too short (minimum is 6 characters")
-        page.should have_content("Password doesn't match confirmation")
-        page.should have_content("Not a valid e-mail format")
-      end
+    it "gives new errors" do
+      current_path.should == '/users'
+      page.should have_content("First Name can't be blank")
+      page.should have_content("Last Name can't be blank")
+      page.should have_content("User name can't be blank")
+      page.should have_content("Password is too short (minimum is 6 characters")
+      page.should have_content("Password doesn't match confirmation")
+      page.should have_content("Not a valid e-mail format")
     end
   end
 end

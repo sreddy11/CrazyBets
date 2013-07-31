a44d31193a2e81dda6e0586335206ba49ec7dd33
sreddy11 <sreddy1.sr@gmail.com>
2013-07-31 12:35:29 -0400

    added spec

--- app/controllers/admin/bets_controller.rb
+++ app/controllers/admin/bets_controller.rb
@@ -1,4 +1,7 @@
 class Admin::BetsController < ApplicationController
+  
+  before_filter :must_be_admin
+
   def new
     @bet = Bet.new
     @bet.build_metric1
@@ -11,7 +14,7 @@ class Admin::BetsController < ApplicationController
   def create
     @bet = Bet.new(bet_params)
     if @bet.save
-      redirect_to current_user
+      redirect_to current_user, :notice => "Bet Created"
     else
       render :new
     end
@@ -22,5 +25,12 @@ class Admin::BetsController < ApplicationController
                                 :metric2_attributes => [:id, :topic]) 
   end
 
+  private
 
+  def must_be_admin
+    unless session[:user_type] == "admin" 
+      flash[:error] = " Unauthorized access"
+      redirect_to(root_url)
+    end 
+  end
 end
--- spec/support/integration_macros.rb
+++ spec/support/integration_macros.rb
@@ -11,7 +11,22 @@ module IntegrationMacros
     fill_in "Confirm Password", :with => "password"
     fill_in "E-mail", :with => "email@yahoo.com"
     click_button "Sign Up"
-    end
+  end
+
+  def login_user
+  end
+  
+  def create_admin
+  end
+
+  def login_admin
+    visit '/'
+    click_link 'Login/Register'
+    fill_in 'sign_in_name', :with => 'sreddy1'
+    fill_in 'sign_in_password', :with => 'password'
+    click_button 'login-button'
+  end
+
 
 
 end
